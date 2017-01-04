d4944a4efdbc4998a47fe5864105ccd63622560a
ShadiJam <shadijam00@gmail.com>
2017-01-04 14:23:09 -0600

    Add photo and description to view

--- app/models/nasa_api.rb
+++ app/models/nasa_api.rb
@@ -2,10 +2,10 @@ class NasaApi < ApplicationRecord
 
 def self.get_api_info
   # "https://api.nasa.gov/planetary/apod?api_key=#{ENG[ 'NASA_API_KEY']}"
-  JSON.parse Net::HTTP.get(URI(
-  "https://api.nasa.gov/planetary/apod?api_key=#{ENV[ 'NASA_API_KEY']}"
-  ))
-  end
+  nasa_api = "https://api.nasa.gov/planetary/apod?api_key=#{ENV[ 'NASA_API_KEY']}"
+  request_to_nasa_api = Net::HTTP.get(URI(nasa_api))
+  JSON.parse request_to_nasa_api
+end
 
 
 end
